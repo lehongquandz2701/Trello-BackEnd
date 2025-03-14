80b6dcc955dbc3372d2ceeb83189d0101aa10434
Simon <lehongquandz2701@gmail.com>
2025-03-14 15:30:13 +0700

    feat: config mongodb

--- src/server.js
+++ src/server.js
@@ -1,32 +1,34 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-console */
 
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import express from "express";
+import { CONNECT_DB, CLOSE_DB } from "./config/mongodb";
+import exitHook from "async-exit-hook";
+import { ENVS } from "./config/environment";
 
-const app = express()
+const START_SERVER = () => {
+  const app = express();
 
-const hostname = 'localhost'
-const port = 8017
+  app.get("/", async (req, res) => {
+    res.end("<h1>Hello World!</h1><hr>");
+  });
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(ENVS.APP_PORT, ENVS.APP_HOST, () => {
+    console.log(
+      `Hello Trung Quan Dev, I am running at ${ENVS.APP_HOST}:${ENVS.APP_PORT}/`
+    );
+  });
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  exitHook(() => {
+    CLOSE_DB();
+  });
+};
+
+CONNECT_DB()
+  .then()
+  .then(async () => {
+    START_SERVER();
+  })
+  .catch((error) => {
+    console.log("error", error);
+    process.exit(0);
+  });
